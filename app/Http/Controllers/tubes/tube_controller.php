0705bd12d9b4cc1d2ec79bba39a2b8bdbae33e11
Shaker Al Muzaini <92122042+1shaker@users.noreply.github.com>
2022-07-08 18:11:32 +0300

    Update tube_controller.php

--- app/Http/Controllers/tubes/tube_controller.php
+++ app/Http/Controllers/tubes/tube_controller.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\tubes;   
  
 use App\Http\Controllers\Controller;   
-use App\Models\tube; 
+use App\Models\tube;  
 use Illuminate\Http\Request; 
  
 class tube_controller extends Controller 
