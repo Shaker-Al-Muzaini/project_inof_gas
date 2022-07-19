7fa46c11b4c50f9e200523022e43f05ee6bba67d
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-07-20 02:07:14 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-    }
+    } 
  
     /**
      * Register the commands for the application.
