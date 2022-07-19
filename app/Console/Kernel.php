a889b1efe1796c68480f5f21e6c2aea3614d8a33
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-07-20 02:06:42 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule; 
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;  
  
-class Kernel extends ConsoleKernel
+class Kernel extends ConsoleKernel 
 {
     /**
      * Define the application's command schedule.
