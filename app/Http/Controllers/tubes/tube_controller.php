a33254f2b534b864413d43ca75b40297e93331e8
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-07-21 22:09:31 +0300

    Update tube_controller.php

--- app/Http/Controllers/tubes/tube_controller.php
+++ app/Http/Controllers/tubes/tube_controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\tubes;     
 use App\Http\Controllers\Controller;      
-use App\Models\tube;  
+use App\Models\tube;   
 use Illuminate\Support\Str;   
 use Illuminate\Http\Request;  
 use Illuminate\Support\Facades\Validator;
